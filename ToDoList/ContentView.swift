ca59d044170a7fc5019a8c6ec01a341b34aa00a3
isabelpt <iaptny@gmail.com>
2025-05-03 10:22:37 -0400

    basic ui

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
     var body: some View {
         VStack {
             HStack {
@@ -16,13 +17,18 @@ struct ContentView: View {
                          .fontWeight(.black)
                 Spacer()
                 Button {
-
+                    withAnimation {
+                        showNewTask = true
+                    }
                 } label: {
                     Text("+")
                         .font(.title)
                             .fontWeight(.bold)
                 }
             }.padding()
+        }
+        if showNewTask {
+            NewToDo()
         }
     }
 }
